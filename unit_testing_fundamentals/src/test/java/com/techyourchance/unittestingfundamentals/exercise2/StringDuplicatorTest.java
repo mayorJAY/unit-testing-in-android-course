96760f6e0a43abae7d978e47009cc2756970b07d
mayorjay <joseolu4gsm@yahoo.com>
2021-03-30 22:28:36 +0100

    feat: Exercise 2

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,29 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
 public class StringDuplicatorTest {
 
+    private StringDuplicator sut;
+
+    @Before
+    public void setUp() {
+        sut = new StringDuplicator();
+    }
+
+    @Test
+    public void duplicate_emptyString_emptyStringReturned() {
+        String result = sut.duplicate("");
+        Assert.assertThat(result, is(""));
+    }
+
+    @Test
+    public void duplicate_anyString_duplicatedStringReturned() {
+        String result = sut.duplicate("string");
+        Assert.assertThat(result, is("stringstring"));
+    }
 }
